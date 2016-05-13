aa23b3e90bcf01a0a13e333fe3a8a6b1d1933099
Developer <klevtsovstanislav@gmial.com>
2016-05-13 23:53:08 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo /etc/init.d/gunicorn restart
